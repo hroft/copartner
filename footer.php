96213fc1e4336b84accfe30be7a9bc12c30d775d
hroft <sagdinov@gmail.com>
2019-02-14 17:03:24 +0300

    template copatrner wp

---
+++ footer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package copartner
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+		<div class="container">
+
+<div class="row row-footer-style  justify-content-around">
+<div class="col text-left col_text_footer">
+<div>ИНН 6168105389</div>
+<div>КПП 616801001</div>
+<div>ОГРН 1186196059103</div>
+<div>ОКПО 35318052</div>
+</div>
+<div class="col text-right col_text_footer">
+<div> г. Ростов-на-Дону,&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<br> пер. Машиностроительный 9А</div>
+<div>почта: amp@copartner.ru&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
